9916fc12e9dcb50c5a6e2da35484de90c92ebada
bryanesteban <bryanesteban_123@hotmail.com>
2024-05-03 05:01:58 -0500

    map fullscreem

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import * as mapboxgl from 'mapbox-gl';
+
 import { MapsRoutingModule } from './maps-routing.module';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
@@ -20,7 +22,7 @@ import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.
     FullScreemPageComponent,
     MarkersPageComponent,
     PropertiesPageComponent,
-    ZoomRangePageComponent
+    ZoomRangePageComponent,
   ],
   imports: [
     CommonModule,
--- src/app/maps/pages/full-screem-page/full-screem-page.component.ts
+++ src/app/maps/pages/full-screem-page/full-screem-page.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
+
+
 
 @Component({
   templateUrl: './full-screem-page.component.html',
   styleUrl: './full-screem-page.component.css'
 })
-export class FullScreemPageComponent {
+export class FullScreemPageComponent implements AfterViewInit {
+
+
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+
+    if(!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      accessToken: 'pk.eyJ1IjoiYnJ5YW5lc3RlYmFuIiwiYSI6ImNsdnBwNmUyYzA0cHIyam1naDA0cmo0cDMifQ.LjzMwPbVrQ2qAKXIOJeCiw',
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+
+  }
 
 }
